7beda22e7bff0e047f66a29aef20060a5bdfd187
jtaylorjohnson <jtaylorjohnson@gmail.com>
2020-04-20 00:31:32 -0500

    adding source lookup

--- lib/picker/cli.rb
+++ lib/picker/cli.rb
@@ -6,7 +6,7 @@ class Cli
         puts "Enter the Name of a City"
         puts "to see the Current Weather!"
         puts " "
-        @input = gets.strip.downcase
+        @input = gets.strip.downcase.split.first
         get_weather(@input)
     end
 
@@ -16,23 +16,30 @@ class Cli
         puts "It is currently #{Place.all[0].weather} in #{Place.all[0].title}."
         puts " "
         puts "To see #{Place.all[0].title}'s current Temperature and Humidity type 'yes'."
-        if gets.strip.downcase == "yes"
+        if gets.strip.downcase == "source"
+            puts "This information comes from #{Place.all[0].source}."
+            new_or_exit
+        elsif gets.strip.downcase == "yes"
             puts " "
             puts "It is Currently #{Place.all[0].temp}°C with a Humidity of #{Place.all[0].humidity}% in #{Place.all[0].title}."
             puts " "
-            puts "Enter another City or 'exit' to Exit."
-            @newinput = gets.strip.downcase
-            if @newinput != "exit"
-            get_weather(@newinput)
-            else puts "Goodbye!"
+            new_or_exit
         else
-            puts "Enter another City or 'exit' to Exit."
-            @newinput = gets.strip.downcase
-            if @newinput != "exit"
-            get_weather(@newinput)
-            else puts "Goodbye!"
+            new_or_exit
         end
     end 
+
+    def new_or_exit
+        puts "Enter another City or 'exit' to Exit."
+        @input = gets.strip.downcase.split.first
+        if @input == "source"
+            puts "This information comes from #{Place.all[0].source}."
+            new_or_exit
+        elsif @input != "exit"
+            get_weather(@input)
+        else puts "Goodbye!"
+        end
+    end
 
 end
  
